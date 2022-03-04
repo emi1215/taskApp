46be15a868660ded4a3eb40e1115f7621e56a6a2
emi <bluming_blue@icloud.com>
2022-03-05 06:07:52 +0900

    エラーの理由がわかりません。

---
+++ taskapp/Task.swift
@@ -0,0 +1,23 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 横田瑛美 on 2022/03/05.
+//
+
+import RealmSwift
+
+class Task: Object {
+    
+    @objc dynamic var id = 0
+    
+    @objc dynamic var title = ""
+    
+    @objc dynamic var contents = ""
+    
+    @objc dynamic var date = Date()
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
